5f26c6af20d618f11e35853e0f7aa5110bdcbdfc
tlenka <topolskamarlena@gmail.com>
2019-09-17 23:31:01 +0200

    up commit

--- SumTheNumbersGameXamarin/SumTheNumbersGameXamarin/ViewModels/SettingsPageViewModel.cs
+++ SumTheNumbersGameXamarin/SumTheNumbersGameXamarin/ViewModels/SettingsPageViewModel.cs
@@ -1,10 +1,61 @@
-﻿using System;
+﻿using SumTheNumbersGameXamarin.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SumTheNumbersGameXamarin.ViewModels
 {
-    class SettingsPageViewModel
+    class SettingsPageViewModel : BaseViewModel
     {
+        private readonly ISettings _settings;
+        private int _count;
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                if (_count != value)
+                {
+                    _count = value;
+                    RaisePropertyChanged(nameof(Count));
+                    _settings.CountOfNumbers = Count;
+                }
+            }
+        }
+
+        public void SomeFun()
+        {
+            //System.Diagnostics.Debug.WriteLine(Count);
+        }
+       
+        //public SettingsModel Settings
+        //{
+        //    get
+        //    {
+        //        return _settings;
+        //    }
+        //    set
+        //    {
+        //        if (_settings != value)
+        //        {
+        //            _settings = value;
+        //            RaisePropertyChanged(nameof(Settings));
+        //        }
+        //    }
+        //}
+
+
+        public SettingsPageViewModel()
+        {
+            _settings = App.GameSettings;
+           // _count = _settings.CountOfNumbers;
+           // System.Diagnostics.Debug.WriteLine(_settings.CountOfNumbers);
+            //_settings = settings;
+            //_settings.CountOfNumbers = 8;
+        }
     }
 }
