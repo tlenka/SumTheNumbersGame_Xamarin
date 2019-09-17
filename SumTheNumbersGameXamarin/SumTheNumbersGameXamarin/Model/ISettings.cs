5ba57d48744026eb876e59644edaacd74a3d8a47
tlenka <topolskamarlena@gmail.com>
2019-09-17 23:27:29 +0200

    Add ISettings and Settings Model

---
+++ SumTheNumbersGameXamarin/SumTheNumbersGameXamarin/Model/ISettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SumTheNumbersGameXamarin.Model
+{
+    public interface ISettings
+    {
+        int CountOfNumbers { get; set; }
+    }
+}
---
+++ SumTheNumbersGameXamarin/SumTheNumbersGameXamarin/Model/SettingsModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SumTheNumbersGameXamarin.Model
+{
+    public class SettingsModel : ISettings
+    {
+        public int CountOfNumbers { get; set; }
+    }
+}
